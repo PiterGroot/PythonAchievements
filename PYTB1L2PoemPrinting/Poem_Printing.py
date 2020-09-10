1bc406bdf4c9c433183e695b4b5eb218e282741e
PiterGroot <70685433+PiterGroot@users.noreply.github.com>
2020-09-10 14:54:25 +0200

    Update Poem_Printing.py

--- PYTB1L2PoemPrinting/Poem_Printing.py
+++ PYTB1L2PoemPrinting/Poem_Printing.py
@@ -9,7 +9,7 @@ vers7 = "Ik zit nu op het Media College."
 vers8 = "Software schrijven is mijn ding,"
 vers9 = "ik heb in dit jaar heel veel zin."
 vers10 = "Nieuwe mensen ontmoeten is erg fijn,"
-vers11 = "daarom schtijf ik nu deze rijm."
+vers11 = "daarom schrijf ik nu deze rijm."
 
 print(vers1, vers2, vers3)
 print(vers4, vers5, vers6)
