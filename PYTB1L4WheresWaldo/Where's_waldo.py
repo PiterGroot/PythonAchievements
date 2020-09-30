e35b85dcef29c614de4a9befeb7fb6e6b21f615f
unknown <31515@ma-web.nl>
2020-09-30 15:28:34 +0200

    Waldo opdracht

--- PYTB1L4WheresWaldo/Where's_waldo.py
+++ PYTB1L4WheresWaldo/Where's_waldo.py
@@ -1,6 +1,16 @@
 import random
+
+nametofind = "Waldo"
+
 people = ["Waldo","Jackson","Ray","Eric","Vaughn","Jesse","Herbert","Robert", "Rodrigo","Elija","Sasha","Nathaniel","Ellie","Allison","Jeremiah", "Paula", "Alisha","Tory","Troy", "Faye"]
 random.shuffle(people)
-for value in variable:
-    pass
-print(people)
+
+for value in people:
+    print(value)
+
+index = people.index(nametofind)
+print("")
+print("<-------------------------------------------------------->")
+print('The index of', nametofind, 'in the list is:', index)
+
+dontclose = input()
